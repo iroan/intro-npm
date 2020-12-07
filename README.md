10650e3b2903d21da4bb8411f2e22bdfbd7133ab
wangkaixuan <2654189525@qq.com>
2020-12-07 12:33:02 +0800

    En: add file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# intro-npm
+# publish
+1. 登录
+1. 发布
+
+# usage 
+**local**
+1. npm install `本项目的绝对路径`
+
---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.wangkaixuanSay = function(){
+    return 'this is my first time use npm to publish package';
+}
